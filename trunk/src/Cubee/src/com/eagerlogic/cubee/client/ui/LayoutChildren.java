058114cc63ec2a8ebe7e8df1636538f4344e9548
David Ipacs <david.ipacs@gmail.com>
2013-09-19 14:18:30 +0200

    LayoutChildren implemented.

--- trunk/src/Cubee/src/com/eagerlogic/cubee/client/ui/LayoutChildren.java
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/ui/LayoutChildren.java
@@ -4,19 +4,61 @@
  */
 package com.eagerlogic.cubee.client.ui;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
  *
  * @author dipacs
  */
-public final class LayoutChildren {
+public final class LayoutChildren implements Iterable<AComponent> {
     
-    private LinkedList<AComponent> children = new LinkedList<AComponent>();
+    private final LinkedList<AComponent> children = new LinkedList<AComponent>();
     private final ALayout parent;
 
     LayoutChildren(ALayout parent) {
         this.parent = parent;
+    }
+    
+    public void add(AComponent component) {
+        if (component == null) {
+            throw new NullPointerException("The component can not be null.");
+        }
+        
+        if (component.getParent() != null || component.getCubeePanel() != null) {
+            throw new IllegalStateException("The component is already a child of a layout.");
+        }
+        
+        component.setParent(parent);
+        children.add(component);
+        parent.onChildrenChanged();
+    }
+    
+    public void remove(AComponent component) {
+        children.remove(component);
+        parent.onChildrenChanged();
+    }
+    
+    public void remove(int index) {
+        children.remove(index);
+        parent.onChildrenChanged();
+    }
+    
+    public void get(int index) {
+        children.get(index);
+    }
+    
+    public int indexOf(AComponent component) {
+        return children.indexOf(component);
+    }
+    
+    public int size() {
+        return children.size();
+    }
+
+    @Override
+    public Iterator<AComponent> iterator() {
+        return children.iterator();
     }
     
 }
