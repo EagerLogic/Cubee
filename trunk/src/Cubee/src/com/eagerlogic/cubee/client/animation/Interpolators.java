bb216bf10e3b1a413ec7992249900cfc5155d996
David Ipacs <david.ipacs@gmail.com>
2013-10-21 08:37:37 +0200

    Interploators are collected in to the Interpolators class.

---
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/animation/Interpolators.java
@@ -0,0 +1,33 @@
+package com.eagerlogic.cubee.client.animation;
+
+/**
+ *
+ * @author dipacs
+ */
+public final class Interpolators {
+	
+	public static final IInterpolator LINEAR = new IInterpolator() {
+
+		@Override
+		public double interpolate(double value) {
+			return value;
+		}
+	};
+	public static final IInterpolator EASE_IN = new IInterpolator() {
+
+		@Override
+		public double interpolate(double value) {
+			return value * value;
+		}
+	};
+	public static final IInterpolator EASE_OUT = new IInterpolator() {
+
+		@Override
+		public double interpolate(double value) {
+			return Math.sqrt(value);
+		}
+	};
+	
+	private Interpolators() {}
+
+}
