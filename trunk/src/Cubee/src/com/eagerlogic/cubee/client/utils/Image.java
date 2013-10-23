86e1c3090856b8ed00143f8b7b24af247f243b97
David Ipacs <david.ipacs@gmail.com>
2013-10-23 21:24:54 +0200

    ImageBackground implemented.

---
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/utils/Image.java
@@ -0,0 +1,19 @@
+package com.eagerlogic.cubee.client.utils;
+
+/**
+ *
+ * @author dipacs
+ */
+public final class Image {
+	
+	private final String url;
+
+	public Image(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+}
