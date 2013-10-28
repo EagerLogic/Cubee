c2e3e6f025d354a136f1a62704d0894c67da74ad
David Ipacs <david.ipacs@gmail.com>
2013-10-28 08:38:58 +0100

    TextArea implemented.

---
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/components/TextArea.java
@@ -0,0 +1,15 @@
+package com.eagerlogic.cubee.client.components;
+
+import com.google.gwt.user.client.DOM;
+
+/**
+ *
+ * @author dipacs
+ */
+public final class TextArea extends TextBox {
+
+	public TextArea() {
+		super(DOM.createTextArea());
+	}
+
+}
