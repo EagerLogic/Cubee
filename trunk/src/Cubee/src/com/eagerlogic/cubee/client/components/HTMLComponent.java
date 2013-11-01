1427243181f3d7d4071abf5b87d49c813bb3e2f6
David Ipacs <david.ipacs@gmail.com>
2013-11-01 13:19:38 +0100

    HTMLComponent implemented

---
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/components/HTMLComponent.java
@@ -0,0 +1,61 @@
+package com.eagerlogic.cubee.client.components;
+
+import com.eagerlogic.cubee.client.properties.BackgroundProperty;
+import com.eagerlogic.cubee.client.properties.BorderProperty;
+import com.eagerlogic.cubee.client.properties.IChangeListener;
+import com.eagerlogic.cubee.client.properties.IntegerProperty;
+import com.eagerlogic.cubee.client.properties.PaddingProperty;
+import com.eagerlogic.cubee.client.properties.Property;
+
+/**
+ *
+ * @author dipacs
+ */
+public final class HTMLComponent extends AUserControl {
+	
+	private final Property<String> html = new Property<String>(null, true, false);
+
+	public HTMLComponent() {
+		html.addChangeListener(new IChangeListener() {
+
+			@Override
+			public void onChanged(Object sender) {
+				if (html.get() == null) {
+					getElement().setInnerHTML("");
+				} else {
+					getElement().setInnerHTML(html.get());
+				}
+			}
+		});
+	}
+
+	public Property<String> htmlProperty() {
+		return html;
+	}
+
+	@Override
+	public IntegerProperty widthProperty() {
+		return super.widthProperty();
+	}
+
+	@Override
+	public IntegerProperty heightProperty() {
+		return super.heightProperty();
+	}
+
+	@Override
+	public BackgroundProperty backgroundProperty() {
+		return super.backgroundProperty();
+	}
+
+	@Override
+	public PaddingProperty paddingProperty() {
+		return super.paddingProperty();
+	}
+
+	@Override
+	public BorderProperty borderProperty() {
+		return super.borderProperty();
+	}
+
+}
