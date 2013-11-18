b690c346f62fe63bd4395d05e4153ef01e00044f
David Ipacs <david.ipacs@gmail.com>
2013-11-18 23:55:54 +0100

    Some color constans added.

--- trunk/src/Cubee/src/com/eagerlogic/cubee/client/styles/Color.java
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/styles/Color.java
@@ -7,6 +7,27 @@ package com.eagerlogic.cubee.client.styles;
 public final class Color {
 	
 	public static final Color BLACK = new Color(0xff000000);
+	public static final Color LIGHT_GRAY = new Color(0xffc0c0c0);
+	public static final Color GRAY = new Color(0xff808080);
+	public static final Color DARK_GRAY = new Color(0xff404040);
+	public static final Color RED = new Color(0xffff0000);
+	public static final Color DARK_RED = new Color(0xff800000);
+	public static final Color GREEN = new Color(0xff00ff00);
+	public static final Color DARK_GREEN = new Color(0xff008000);
+	public static final Color GRASS_GREEN = new Color(0xff90ff00);
+	public static final Color DARK_BLUE = new Color(0xff000080);
+	public static final Color BLUE = new Color(0xff0000ff);
+	public static final Color YELLOW = new Color(0xffffff00);
+	public static final Color CYAN = new Color(0xff00ffff);
+	public static final Color PURPLE = new Color(0xffff00ff);
+	public static final Color ORANGE = new Color(0xffff8000);
+	public static final Color RED_ORANGE = new Color(0xffff4000);
+	public static final Color SKY_BLUE = new Color(0xff0080ff);
+	public static final Color FUNKY_BLUE = new Color(0xff00c0ff);
+	public static final Color PINK_PURPLE = new Color(0xffff0080);
+	public static final Color BLUE_PURPLE = new Color(0xffff00c0);
+	public static final Color BROWN = new Color(0xff603000);
+	public static final Color PINK = new Color(0xffff8080);
 	public static final Color WHITE = new Color(0xffffffff);
 	public static final Color TRANSPARENT = new Color(0x00000000);
 	
