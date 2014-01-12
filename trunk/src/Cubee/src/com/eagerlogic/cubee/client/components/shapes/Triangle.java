038b976e1bb14363214e6e7df7c47c232612ae1d
David Ipacs <david.ipacs@gmail.com>
2014-01-12 13:14:34 +0100

    Triangle fixed

--- trunk/src/Cubee/src/com/eagerlogic/cubee/client/components/shapes/Triangle.java
+++ trunk/src/Cubee/src/com/eagerlogic/cubee/client/components/shapes/Triangle.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.eagerlogic.cubee.client.components.shapes;
 
+import com.eagerlogic.cubee.client.components.AComponent;
 import com.eagerlogic.cubee.client.components.AUserControl;
 import com.eagerlogic.cubee.client.properties.AExpression;
 import com.eagerlogic.cubee.client.properties.ColorProperty;
@@ -14,59 +14,74 @@ import com.eagerlogic.cubee.client.properties.Property;
 import com.eagerlogic.cubee.client.styles.Border;
 import com.eagerlogic.cubee.client.styles.Color;
 import com.eagerlogic.cubee.client.styles.EOrientation;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.DOM;
 
 /**
  *
  * @author dipacs
  */
-public final class Triangle extends AUserControl {
-    
+public final class Triangle extends AComponent {
+
     private final Property<EOrientation> orientation = new Property<EOrientation>(EOrientation.BOTTOM, false, false);
     private final ColorProperty color = new ColorProperty(Color.RED, false, false);
     private final IntegerProperty width = new IntegerProperty(100, false, false);
     private final IntegerProperty height = new IntegerProperty(100, false, false);
 
     public Triangle() {
+        super(DOM.createDiv());
+        getElement().getStyle().setWidth(0, Style.Unit.PX);
+        getElement().getStyle().setHeight(0, Style.Unit.PX);
         this.borderProperty().bind(new AExpression<Border>() {
 
             {
                 bind(orientation, color, width, height);
             }
-            
+
             @Override
             public Border calculate() {
-               EOrientation ori = orientation.get();
-               int lb = 0;
-               int tb = 0;
-               int rb = 0;
-               int bb = 0;
-               switch (ori) {
-                   case LEFT:
-                       rb = 0;
-                       tb = width.get() / 2;
-                       bb = width.get() - lb;
-                       lb = height.get();
-                       break;
-                   case TOP:
-                       bb = 0;
-                       lb = width.get() / 2;
-                       rb = width.get() - lb;
-                       tb = height.get();
-                       break;
-                   case RIGHT:
-                       lb = 0;
-                       tb = width.get() / 2;
-                       bb = width.get() - lb;
-                       rb = height.get();
-                       break;
-                   case BOTTOM:
-                       tb = 0;
-                       lb = width.get() / 2;
-                       rb = width.get() - lb;
-                       bb = height.get();
-                       break;
-               }
-               return new Border(lb, tb, rb, bb, color.get(), color.get(), color.get(), color.get(), 0, 0, 0, 0);
+                EOrientation ori = orientation.get();
+                int leftWidth = 0;
+                int topWidth = 0;
+                int rightWidth = 0;
+                int bottomWidth = 0;
+                Color leftColor = Color.TRANSPARENT;
+                Color topColor = Color.TRANSPARENT;
+                Color rightColor = Color.TRANSPARENT;
+                Color bottomColor = Color.TRANSPARENT;
+                switch (ori) {
+                    case LEFT:
+                        leftWidth = 0;
+                        topWidth = height.get() / 2;
+                        bottomWidth = height.get() - leftWidth;
+                        rightWidth = width.get();
+                        rightColor = color.get();
+                        break;
+                    case TOP:
+                        topWidth = 0;
+                        leftWidth = width.get() / 2;
+                        rightWidth = width.get() - leftWidth;
+                        bottomWidth = height.get();
+                        bottomColor = color.get();
+                        break;
+                    case RIGHT:
+                        rightWidth = 0;
+                        topWidth = height.get() / 2;
+                        bottomWidth = height.get() - leftWidth;
+                        leftWidth = width.get();
+                        leftColor = color.get();
+                        break;
+                    case BOTTOM:
+                        bottomWidth = 0;
+                        leftWidth = width.get() / 2;
+                        rightWidth = width.get() - leftWidth;
+                        topWidth = height.get();
+                        topColor = color.get();
+                        break;
+                }
+                getElement().getStyle().setWidth(0, Style.Unit.PX);
+                getElement().getStyle().setHeight(0, Style.Unit.PX);
+                return new Border(leftWidth, topWidth, rightWidth, bottomWidth, leftColor, topColor, rightColor, bottomColor, 0, 0, 0, 0);
             }
         });
     }
@@ -95,34 +110,28 @@ public final class Triangle extends AUserControl {
         return color;
     }
 
-    @Override
     public void setHeight(Integer height) {
         this.height.set(height);
     }
 
-    @Override
     public Integer getHeight() {
         return this.height.get();
     }
 
-    @Override
     public void setWidth(Integer width) {
         this.width.set(width);
     }
 
-    @Override
     public Integer getWidth() {
         return this.width.get();
     }
 
-    @Override
     public IntegerProperty heightProperty() {
-        return width;
+        return height;
     }
 
-    @Override
     public IntegerProperty widthProperty() {
         return width;
     }
-    
+
 }
