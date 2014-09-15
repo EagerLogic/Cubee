97a8b5217a7c505aab4943a3753107bc8a87c043
David Ipacs <david.ipacs@gmail.com>
2014-09-15 10:54:58 +0200

    DOM helper for creating elements with NS parameter.

---
+++ src/Cubee/src/main/java/com/eagerlogic/cubee/client/utils/DOMHelper.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 dipacs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.eagerlogic.cubee.client.utils;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+
+/**
+ *
+ * @author dipacs
+ */
+public class DOMHelper {
+    
+    public static native Element createElementNS(Document document, String namespaceURI, String qualifiedName) /*-{
+        return document.createElementNS(namespaceURI, qualifiedName);
+    }-*/;
+
+    private DOMHelper() {
+    }
+    
+}
